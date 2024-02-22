c00475d3982d1c21b943300570870ac8a4bdfeda
BalaMurali R <121211875+Balamurali03@users.noreply.github.com>
2024-02-22 15:50:24 +0530

    Add files via upload

---
+++ appAuthunticate/src/main/java/com/project/security/appAuthunticate/AppAuthunticateApplication.java
@@ -0,0 +1,13 @@
+package com.project.security.appAuthunticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAuthunticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAuthunticateApplication.class, args);
+	}
+
+}
---
+++ appAuthunticate/src/test/java/com/project/security/appAuthunticate/AppAuthunticateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.security.appAuthunticate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAuthunticateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
